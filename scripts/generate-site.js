7f56df26654165958e475735872a50da86ebf490
Eric Lifka <eric.lifka@pnmac.com>
2024-09-14 20:40:17 -0400

    first pass at generating the html with recipe data included

---
+++ scripts/generate-site.js
@@ -0,0 +1,102 @@
+const { copyFile, readdir, readFile, writeFile } = require('node:fs/promises')
+
+const loadRecipes = async () => {
+  let recipes = [ ]
+
+  for (let group of await readdir("./recipes")) {
+    let recipe_group = { name: group, recipes: [] }
+    
+    for (let recipe of await readdir(`./recipes/${group}`)) {
+      let contents = await readFile(`./recipes/${group}/${recipe}`, { encoding: "utf-8" })
+      let [name, ...steps] = contents.split('\n')
+
+      recipe_group.recipes.push({ name, steps })
+    }
+
+    recipes.push(recipe_group)
+  }
+
+  return recipes
+}
+
+const generateContent = recipes => `
+  <main>
+    ${recipes.map( group => `
+      <details>
+        <summary>${group.name}</summary>
+        ${group.recipes.map(generateRecipe).join('')}
+      </details>
+    `).join('')}
+  </main>`
+
+const generateRecipe = recipe => `
+  <details>
+    <summary>${recipe.name}</summary>
+    <ul>
+      ${recipe.steps.map( step => `
+        <li>${step}</li>
+      `).join('')}
+    </ul>
+  </details>`
+
+const run = async () => {
+  await copyFile("./site-template/script.js", "./website/script.js")
+  await copyFile("./site-template/style.css", "./website/style.css")
+
+  let html_template = await readFile("./site-template/index.html", { encoding: "utf-8" })
+  let [head, foot] = html_template.split("{{--CONTENT--}}")
+  let content = generateContent(await loadRecipes())
+  let html = [head, content, foot].join('\n')
+
+  await writeFile(`./website/index.html`, html)
+}
+
+`
+<main>
+    <details>
+      <summary>Apps</summary>
+      
+      <details>
+        <summary>Recipe 1</summary>
+        <ul>
+          <li>ingredient 1</li>
+          <li>ingredient 2</li>
+          <li>step 1</li>
+          <li>step 2</li>
+        </ul>
+      </details>
+      <details>
+        <summary>Recipe 2</summary>
+        <ul>
+          <li>ingredient 1</li>
+          <li>ingredient 2</li>
+          <li>step 1</li>
+          <li>step 2</li>
+        </ul>
+      </details>
+    </details>
+    <details>
+      <summary>Bread</summary>
+      <details>
+        <summary>Recipe 1</summary>
+        <ul>
+          <li>ingredient 1</li>
+          <li>ingredient 2</li>
+          <li>step 1</li>
+          <li>step 2</li>
+        </ul>
+      </details>
+      <details>
+        <summary>Recipe 2</summary>
+        <ul>
+          <li>ingredient 1</li>
+          <li>ingredient 2</li>
+          <li>step 1</li>
+          <li>step 2</li>
+        </ul>
+      </details>
+    </details>
+  </main>
+  `
+
+run()
